b8341413b0874394a4d36bfe6b62dea6a1b582c7
Daphna Shezaf <daphna@shezaf.com>
2016-06-22 22:04:21 +0100

    added train_station_visits

--- station_service_statistics.py
+++ station_service_statistics.py
@@ -15,7 +15,35 @@ def group(iterable, key_func):
     return res
 
 
-def station_visits(g, output_folder, start_date, end_date, max_distance_from_station=500):
+# station_hourly_data Dict[int, Tuple[int, int]] - dictionary from station id to (hour, count)
+def export_station_hourly_data(g, station_hourly_data, output_filename):
+    print("Running export_station_hourly_data")
+    with open(output_filename, 'w', encoding='utf8') as f:
+        field_names = ['station_stop_id', 'station_name', 'daily_total'] + [('h%d' % h) for h in range(26)]
+        f.write(','.join(field_names) + '\n')
+        for station_id in station_hourly_data:
+            station = g.stops[station_id]
+            line = ','.join([str(station_id), station.stop_name] +
+                            [str(sum(station_hourly_data[station_id]))] +
+                            [str(count) for count in station_hourly_data[station_id]])
+            f.write(line + '\n')
+
+
+# station_to_hourly_counter: Dict[int, Counter[Tuple[int, int]]] -
+#    for each station, a counter of (day, hour) pairs
+# returns -  dictionary from station id to (hour, count)
+def station_hourly_average_sun_to_thurs(counters):
+    print("Running station_hourly_average_sun_to_thurs")
+    result = {}
+    for station in counters:
+        result[station] = []
+        for hour in range(26):
+            hourly_average = sum(counters[station][(day, hour)] for day in (6, 0, 1, 2, 3)) / 5
+            result[station].append(hourly_average)
+    return result
+
+
+def bus_station_visits(g, output_folder, start_date, end_date, max_distance_from_station=500):
     def find_stops_near_station(route_story):
         stops_near_stations = (stop for stop in route_story.stops
                                if g.stops[stop.stop_id].train_station_distance < max_distance_from_station)
@@ -49,35 +77,34 @@ def station_visits(g, output_folder, start_date, end_date, max_distance_from_sta
         print("  total number of buses stopping near train stations in a week %d" % total_visits)
         return station_to_hourly_counter
 
-    def station_hourly_average_sun_to_thurs(station_to_hourly_counter):
-        print("Running station_hourly_average_sun_to_thurs")
-        result = {}
-        for station in station_to_hourly_counter:
-            result[station] = []
-            for hour in range(26):
-                hourly_average = sum(station_to_hourly_counter[station][(day, hour)] for day in (6, 0, 1, 2, 3)) / 5
-                result[station].append(hourly_average)
-        return result
+    output_filename = os.path.join(output_folder, 'hourly_bus_station_visit_sun_thur_%s_%s.txt' %
+                                   (start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
+    export_station_hourly_data(g, station_hourly_average_sun_to_thurs(find_per_station_day_hour()), output_filename)
 
-    def export_station_hourly_data(station_hourly_data):
-        print("Running export_station_hourly_data")
-        output_filename = os.path.join(output_folder, 'hourly_station_visit_sun_thur_%s_%s.txt' %
-                                       (start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
-        with open(output_filename, 'w', encoding='utf8') as f:
-            field_names = ['station_stop_id', 'station_name', 'daily_total'] + [('h%d' % h) for h in range(26)]
-            f.write(','.join(field_names) + '\n')
-            for station_id in station_hourly_data:
-                station = g.stops[station_id]
-                line = ','.join([str(station_id), station.stop_name] +
-                                [str(sum(station_hourly_data[station_id]))] +
-                                [str(count) for count in station_hourly_data[station_id]])
-                f.write(line + '\n')
 
-    export_station_hourly_data(station_hourly_average_sun_to_thurs(find_per_station_day_hour()))
+def train_station_visits(g, output_folder, start_date, end_date):
+    print("Running train_station_visits")
+    train_trips = (trip for trip in g.trips.values() if trip.route.route_type == 2)
+    train_trips = [trip for trip in train_trips if
+                   trip.service.end_date >= start_date and trip.service.start_date <= end_date]
+    print("There are %s trips in the date span" % len(train_trips))
+    print("Building hourly data dict")
+    station_to_hourly_counter = defaultdict(lambda: Counter())
+    for trip in train_trips:
+        for stop in trip.route_story.stops:
+            hour = (trip.start_time + stop.arrival_offset) // 3600
+            for day in trip.service.days:
+                station_to_hourly_counter[g.stops[stop.stop_id].nearest_train_station_id][(day, hour)] += 1
+    print("  done. found data for %d stations" % len(station_to_hourly_counter))
+    print("exporting")
+    output_filename = os.path.join(output_folder, 'hourly_train_arrivals_sun_thur_%s_%s.txt' %
+                                   (start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
+    export_station_hourly_data(g, station_hourly_average_sun_to_thurs(station_to_hourly_counter), output_filename)
+    print("   done.")
 
 
 if __name__ == '__main__':
     gtfs = ExtendedGTFS(r'data/gtfs_2016_05_25/israel-public-transportation.zip')
     gtfs.load_stops()
     gtfs.load_trips()
-    station_visits(gtfs, r'data/gtfs_2016_05_25/', date(2016, 6, 1), date(2016, 6, 14))
+    train_station_visits(gtfs, r'data/gtfs_2016_05_25/', date(2016, 6, 1), date(2016, 6, 14))
